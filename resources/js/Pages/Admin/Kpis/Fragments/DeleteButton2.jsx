1125c4c90cb36f19ff6dc9eb003f5f7c1660e9c5
JohanFR11 <jeruiz@meltec.com.co>
2024-11-05 17:38:36 -0500

    Se sube solucion de documentos ya se pueden descargar y borrar, y las kpi ya se pueden borrar

---
+++ resources/js/Pages/Admin/Kpis/Fragments/DeleteButton2.jsx
@@ -0,0 +1,47 @@
+import { Button } from '@nextui-org/react';
+import { showAlert, handleSwalError, handleSwalSuccess } from '@/helpers/swalHelper';
+import axios from 'axios';
+
+export default function DeleteButton({ id }) {
+  const handleDelete = async (id) => {
+    showAlert({
+        title: 'Advertencia',
+        icon: 'warning',
+        text: '¿Seguro desea eliminar el elemento?'
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            try {
+                const request = await axios.delete(`/kpi-reports/${id}`, {
+                    headers: {
+                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                    }
+                });
+
+                if (request.status !== 200) {
+                    throw new Error(request.data.message);
+                } else {
+                    handleSwalSuccess({ message: request.data.message }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.reload();
+                        }
+                    });
+                }
+            } catch (error) {
+                handleSwalError({ message: error.response?.data?.message || error.message });
+            }
+        }
+    });
+};
+
+
+  return (
+    <Button
+      className="text-white bg-red-800 px-5 py-2 rounded-lg mx-1 hover:bg-red-600 transition ease-out"
+      color="danger"
+      size="sm"
+      onPress={() => handleDelete(id)}
+    >
+      Borrar
+    </Button>
+  );
+}
