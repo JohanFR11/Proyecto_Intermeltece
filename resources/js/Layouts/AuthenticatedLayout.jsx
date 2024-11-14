c4ca3dbe1497b9b925771795cf80973c01b69e99
CamiloSTDev <camitotena@gmail.com>
2024-11-14 09:10:29 -0500

    se configuro para que el desplegable aparezca al lado del modulo y sean desplegables independientes

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable no-undef */
-import { HomeIcon, DatacenterIcon, ToolIcon, SellerIcon, HumanIcon, AccountingIcon } from '@/Components/icons/Icons'
+import { HomeIcon, DatacenterIcon, ToolIcon, SellerIcon, HumanIcon, AccountingIcon,ReportIcon } from '@/Components/icons/Icons'
 import Sidebar, { SidebarItem } from './partials/Sidebar'
 import DevMessage from './partials/DevMessage'
 import TopBar from './partials/TopBar'
 import NavKpis from '@/routes/Kpis/NavKpis'
-import NavDropdown from '@/Components/NavDropdown';
 import { useState } from 'react'
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 
 export default function Authenticated ({ auth, header, children, unreadNotifications }) {
   const user = auth.user
 
-  const [isKpiMenuOpen, setIsKpiMenuOpen] = useState(false); // Estado para el subíndice
+  const [openMenus, setOpenMenus] = useState({}); // Estado de submenús individuales
 
-  const toggleKpiMenu = () => {
-    setIsKpiMenuOpen(!isKpiMenuOpen); // Alternar entre abierto y cerrado
+  const toggleKpiMenu = (menuId) => {
+    setOpenMenus((prevState) => ({
+      ...prevState,
+      [menuId]: !prevState[menuId] // Alterna solo el submenú seleccionado
+    }));
   };
   return (
     <div id='app2' className='min-h-screen bg-gray-100 gap-2'>
@@ -25,26 +27,43 @@ export default function Authenticated ({ auth, header, children, unreadNotificat
         {
           user.roles[0].name === 'Administrador' ? (<SidebarItem icon={<DatacenterIcon size='32px' color='#395181' />} href={route('admin.parts.index')} text='Datacenter Meltec IT' />) : ''
         }
-
-        <SidebarItem icon={<SellerIcon size='32px' color='#395181' />} href={route('commercial.quoter')} text='Area Comercial' />
         <div className="flex items-center">
-        <span onClick={toggleKpiMenu} className="cursor-pointer">
-          {isKpiMenuOpen ? <FaChevronUp size={20} color="#395181" /> : <FaChevronDown size={20} color="#395181" />}
-        </span>
-      </div>
-      {isKpiMenuOpen && (
-          <NavKpis category={1} /> 
+          <SidebarItem icon={<SellerIcon size='32px' color='#395181' />} href={route('commercial.quoter')} text='Area Comercial' />
+          <div className="flex items-center">
+          <span onClick={() => toggleKpiMenu('comercial')} className="cursor-pointer ml-2">
+            {openMenus['comercial'] ? <FaChevronUp size={20} color="#395181" /> : <FaChevronDown size={20} color="#395181" />}
+          </span>
+          </div>
+          
+        </div>
+
+        {openMenus['comercial'] && (
+              <div className='[grid-area:aside] max-w-xs'>
+                <NavKpis category={1} />
+              </div>
           )}
-        
         <SidebarItem icon={<HumanIcon size='32px' color='#395181' />} href={route('resources.hseq.index')} text='Area HSEQ' />
         <SidebarItem icon={<AccountingIcon size='32px' color='#395181' />} href={route('payments.index')} text='Area Contable' />
         {
           user.roles[0].name === 'Administrador' ? (<SidebarItem icon={<ToolIcon size='32px' color='#395181' />} href={route('admin.users.index')} text='Administrador del Sistema' />) : ''
         }
+         <div className="flex items-center">
+          <SidebarItem icon={<ReportIcon size='32px' color='#395181' />} text="Kpi's" />
+          <div className="flex items-center">
+          <span onClick={() => toggleKpiMenu('kpis')} className="cursor-pointer ml-2">
+            {openMenus['kpis'] ? <FaChevronUp size={20} color="#395181" /> : <FaChevronDown size={20} color="#395181" />}
+          </span>
+          </div>
+          
+        </div>
 
-        <NavKpis category={2} />
-        <NavKpis category={3} />
-        <NavKpis category={4} />
+        {openMenus['kpis'] && (
+              <div className='[grid-area:aside] max-w-xs'>
+                <NavKpis category={2} />
+                <NavKpis category={3} />
+                <NavKpis category={4} />
+              </div>
+          )}
 
       </Sidebar>
 
