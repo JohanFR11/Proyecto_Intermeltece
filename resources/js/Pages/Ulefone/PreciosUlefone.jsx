247966fb10748e6eb78a6130bed064e917e8670c
JohanFR11 <jeruiz@meltec.com.co>
2025-01-27 09:59:20 -0500

    Ya se tiene el logo

--- resources/js/Pages/Ulefone/PreciosUlefone.jsx
+++ resources/js/Pages/Ulefone/PreciosUlefone.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useDisclosure, Button, Input, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from '@nextui-org/react'
+import { useDisclosure} from '@heroui/react'
 import axios from 'axios';
 import ModalDataModelo from './Components/ModalInfoModelo';
+import Ulefone from './Image/Ulefone.png';
 
 const PreciosUlefone = ({ data, odata }) => {
     const isDataAvailable = Array.isArray(data) && data.length > 0;
@@ -72,49 +73,54 @@ const PreciosUlefone = ({ data, odata }) => {
         console.log('datos del datosmodel',DatosModelo) */
 
     return (
-        <div className="container mx-auto my-8 p-4">
-            <table className="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
-                <thead className="bg-blue-50 text-gray-600 uppercase text-xs leading-normal">
-                    <tr>
-                        <th className="py-3 px-4 text-center">Modelo</th>
-                        <th className="py-3 px-4 text-center">Stock Disponible</th>
-                        <th className="py-3 px-4 text-center">Moneda</th>
-                        <th className="py-3 px-4 text-center">Precio Unitario Canal Platinum</th>
-                        <th className="py-3 px-4 text-center">Precio Unitario Canal Gold</th>
-                        <th className="py-3 px-4 text-center">Precio Unitario Canal Silver</th>
-                        <th className="py-3 px-4 text-center">Precio Unitario Seguridad Física/Corporativo</th>
-                        <th className="py-3 px-4 text-center">Precio Unitario Cliente Final</th>
-                    </tr>
-                </thead>
-                <tbody className="text-gray-700">
-                    {isDataAvailable ? (
-                        data.map((item, index) => (
-                            <tr key={index} className={`${index % 2 === 0 ? 'bg-gray-50' : 'bg-white'} hover:bg-gray-100`}>
-                                <td>
-                                    {sizes.map((size) => (
-                                        <button className="py-3 px-4 text-left w-[400px]" onClick={() => handleOpen(item.MODELO, size)}> {item.MODELO} </button>
-                                    ))}
-                                </td>
-                                <td className="py-3 px-4 text-center w-[30px]">{parseInt(stockData[item.MODELO] !== undefined ? stockData[item.MODELO] : 0)}</td>
-                                <td className="py-3 px-4 text-center">{item.MONEDA}</td>
-                                <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item.PRECIO_UNITARIO_CANAL_PLATINUM_Facturacion_Mayor_a_100_Millones)}</td>
-                                <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item[precioGold])}</td>
-                                <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item[precioSilver])}</td>
-                                <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item.PRECIO_UNITARIO_SEGURIDAD_FISICA_O_CORPORATIVO_PAGO_A_CREDITO)}</td>
-                                <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item.PRECIO_UNITARIO_CLIENTE_FINAL)}</td>
-                            </tr>
-                        ))
-                    ) : (
+        <div className="items-center">
+            <div className="flex justify-center">
+                <a href='https://ulefone.com.co/?gad_source=1&gclid=CjwKCAiA-ty8BhA_EiwAkyoa3wqdtfbAo4goLrUBlfddtpXx8K9nMyC_J3tVn5dyPF1O66GowGf2RBoC7HIQAvD_BwE'><img className="rounded-3xl shadow-2xl scale-95 transition-transform duration-300 hover:scale-110" src={Ulefone} width={300} height={50}/></a>
+            </div>
+            <div className="mr-4 p-4 overflow-x-auto">
+                <table className=" bg-white border border-gray-300 rounded-lg shadow-md">
+                    <thead className="bg-blue-50 text-gray-600 uppercase text-xs leading-normal">
                         <tr>
-                            <td colSpan="8" className="text-center py-3 px-4">
-                                No hay datos disponibles.
-                            </td>
+                            <th className="py-3 px-4 text-center">Modelo</th>
+                            <th className="py-3 px-4 text-center">Stock Disponible</th>
+                            <th className="py-3 px-4 text-center">Moneda</th>
+                            <th className="py-3 px-4 text-center">Precio Unitario Canal Platinum</th>
+                            <th className="py-3 px-4 text-center">Precio Unitario Canal Gold</th>
+                            <th className="py-3 px-4 text-center">Precio Unitario Canal Silver</th>
+                            <th className="py-3 px-4 text-center">Precio Unitario Seguridad Física/Corporativo</th>
+                            <th className="py-3 px-4 text-center">Precio Unitario Cliente Final</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody className="text-gray-700">
+                        {isDataAvailable ? (
+                            data.map((item, index) => (
+                                <tr key={index} className={`${index % 2 === 0 ? 'bg-gray-50' : 'bg-white'} hover:bg-gray-100`}>
+                                    <td>
+                                        {sizes.map((size) => (
+                                            <button className="py-3 px-4 text-left w-[400px]" onClick={() => handleOpen(item.MODELO, size)}> {item.MODELO} </button>
+                                        ))}
+                                    </td>
+                                    <td className="py-3 px-4 text-center w-[30px]">{parseInt(stockData[item.MODELO] !== undefined ? stockData[item.MODELO] : 0)}</td>
+                                    <td className="py-3 px-4 text-center">{item.MONEDA}</td>
+                                    <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item.PRECIO_UNITARIO_CANAL_PLATINUM_Facturacion_Mayor_a_100_Millones)}</td>
+                                    <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item[precioGold])}</td>
+                                    <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item[precioSilver])}</td>
+                                    <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item.PRECIO_UNITARIO_SEGURIDAD_FISICA_O_CORPORATIVO_PAGO_A_CREDITO)}</td>
+                                    <td className="py-3 px-4 text-center">{new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(item.PRECIO_UNITARIO_CLIENTE_FINAL)}</td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="8" className="text-center py-3 px-4">
+                                    No hay datos disponibles.
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
 
-            <ModalDataModelo modeloUlefone={modeloUlefone} size={size} open={isOpen} close={onClose} />
+                <ModalDataModelo modeloUlefone={modeloUlefone} size={size} open={isOpen} close={onClose} />
+            </div>
         </div>
     );
 };
